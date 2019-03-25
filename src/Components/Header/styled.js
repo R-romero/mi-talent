a8e33e556b7c355e684242814222d8f605be78dd
R-romero <r.romero@outlook.com.br>
2019-03-25 15:52:31 -0300

    logic changes for toggle

--- src/Components/Header/styled.js
+++ src/Components/Header/styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
 export const Wrap = styled.div `
-display: flex;
-margin-top: 25px;
-margin-right: 25px;
-margin-left: 25px; 
-justify-content: space-between;
+  display: flex;
+  margin-top: 25px;
+  margin-right: 25px;
+  margin-left: 25px;
+  height: 100px; 
+  justify-content: space-between;
 `
 
 export const Navigation = styled.ul `
@@ -16,13 +17,38 @@ export const Navigation = styled.ul `
   margin-inline-start: 0px;
   margin-inline-end: 0px;
   padding-inline-start: 0;
+  width: 250px;
+`
+
+export const NavItem = styled.li `
+    margin-right: 40px;
+    color: "#1a1a1d";
 `
 
 export const Item = styled.li `
   margin-right: 40px;
-
   font-style: 'Nunito';
   font-weight: bolder;
   font-size: 14px;
   color: "#1a1a1d";
+  display: ${({active}) => active ? 'block' : 'none'};
+`
+
+export const SearchBar = styled.div `
+  display: flex;
+  justify-content:flex-end;
+  width: 200px;
+  margin-top: 3px;
+`
+
+export const Search = styled.input `
+  padding-top: 3px;
+  margin-right: 2px;
+  font-style: 'Nunito';
+  font-weight: bolder;
+  font-size: 14px;
+  color: #8c8c8e;
+  height: 15px;
+  border: none;
+  display: ${({active}) => active ? "block" : "none"};
 `
