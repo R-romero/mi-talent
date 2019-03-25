244a844cc1f5d9f73ff473ddc5e87cd4fcc5db2a
R-romero <r.romero@outlook.com.br>
2019-03-25 15:52:08 -0300

    added search and nav toggle

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,37 +1,55 @@
 import React, { Component } from "react";
-import { Wrap, Navigation, Item } from "./styled";
+import { Wrap, Navigation, Item, SearchBar, Search, NavItem } from "./styled";
 import Logo from "../Logo/logo";
 
 class Header extends Component {
+
+  state = {
+    isSearchActive: false,
+    isNavActive: true
+  }
+
+  SearchHandler = () => {
+    this.setState({
+      isSearchActive: !this.state.isSearchActive
+    })
+  }
+
+  NavHandler = () => {
+    this.setState({
+      isNavActive: !this.state.isNavActive
+    })
+  }
+
   render() {
     return (
       <Wrap>
         <nav>
           <Navigation>
-            <Item>
+            <NavItem onClick={this.NavHandler}>
               <svg
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="#1a1a1d"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <line x1="3" y1="12" x2="21" y2="12" />
                 <line x1="3" y1="6" x2="21" y2="6" />
                 <line x1="3" y1="18" x2="21" y2="18" />
               </svg>
-            </Item>
-            <Item>Clients</Item>
-            <Item>News</Item>
+            </NavItem>
+            <Item active={this.state.isNavActive}>Clients</Item>
+            <Item active={this.state.isNavActive}>News</Item>
           </Navigation>
         </nav>
 
         <Logo />
-        <div>
-          <input type="text" />
+        <SearchBar>
+          <Search type="text" placeholder="SEARCH POST"  active={this.state.isSearchActive}/>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -39,15 +57,16 @@ class Header extends Component {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-search"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-search"
+            onClick={this.SearchHandler}
           >
             <circle cx="10.5" cy="10.5" r="7.5" />
             <line x1="21" y1="21" x2="15.8" y2="15.8" />
           </svg>
-        </div>
+        </SearchBar>
       </Wrap>
     );
   }
